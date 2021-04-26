f1d269d038a213139d382aafd2f356276a02ef44
alexanderlitonjua <alexander.litonjua150418@my.lsgh.edu.ph>
2021-04-27 07:40:59 +0800

    1 file modified

--- pages/home/home.ts
+++ pages/home/home.ts
@@ -23,19 +23,15 @@ export class HomePage {
     calculateLOAN() {
 
       this.downpaymentamount = this.price*this.downpayment / 100;
-      this.downpaymentamount = parseFloat(this.downpaymentamount.toFixed(2));
-
       this.loanamount = this.price - this.downpaymentamount;
-      this.loanamount = parseFloat(this.loanamount.toFixed(2));
-
       this.interest= 0.3026;
-
       this.monthlypayment = (this.loanamount + this.interest) / this.months;
-      this.monthlypayment = parseFloat(this.monthlypayment.toFixed(2));
-
-
       this.totalinterest = (this.loanamount * this.interest) / 100;
-      this.totalinterest = parseFloat(this.totalinterest.toFixed(2));
       
+      this.downpaymentamount = parseFloat(this.downpaymentamount.toFixed(2));
+      this.loanamount = parseFloat(this.loanamount.toFixed(2));
+      this.monthlypayment = parseFloat(this.monthlypayment.toFixed(2));
+      this.totalinterest = parseFloat(this.totalinterest.toFixed(2));
+
     }
 }
