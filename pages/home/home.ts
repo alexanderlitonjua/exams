a43e24f938724ede61291403fde963cbfd4c5cc1
alexanderlitonjua <alexander.litonjua150418@my.lsgh.edu.ph>
2021-04-29 11:09:46 +0800

    2 files modified

--- pages/home/home.ts
+++ pages/home/home.ts
@@ -10,28 +10,19 @@ export class HomePage {
   price : number;
   months: number;
   downpayment: number;
-  
   loanamount: number;
   monthlypayment: number;
   downpaymentamount: number;
   interest:number;
   totalinterest:number;
-  
+    constructor(public navCtrl: NavController) {}
+    calculateLoan() {
 
-  constructor(public navCtrl: NavController) {}
-
-    calculate() {
-
-      this.downpaymentamount = this.price*this.downpayment / 100;
-      this.loanamount = this.price - this.downpaymentamount;
-      this.interest= 0.3026;
-      this.monthlypayment = (this.loanamount + this.interest) / this.months;
-      this.totalinterest = (this.loanamount * this.interest) / 100;
-      
-      this.downpaymentamount = parseFloat(this.downpaymentamount.toFixed(2));
-      this.loanamount = parseFloat(this.loanamount.toFixed(2));
-      this.monthlypayment = parseFloat(this.monthlypayment.toFixed(2));
-      this.totalinterest = parseFloat(this.totalinterest.toFixed(2));
+      this.downpaymentamount = (this.price*this.downpayment) / 100
+      this.loanamount = (this.price - this.downpaymentamount) 
+      this.interest= 0.3026
+      this.monthlypayment = (this.loanamount + this.interest) / this.months
+      this.totalinterest = (this.loanamount * this.interest) / 100
 
     }
 }
